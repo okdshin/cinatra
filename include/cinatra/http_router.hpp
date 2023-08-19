b9e90a02fee539311ee0ab4b3a25781b1ed4e09d
okada <kokuzen@gmail.com>
2023-08-19 22:59:34 +0900

    cast to std::string

--- include/cinatra/http_router.hpp
+++ include/cinatra/http_router.hpp
@@ -54,7 +54,7 @@ public:
         wild_card
             ? url
             : std::string_view{method.data(), method.size() + url.size() + 1};
-    auto it = map_invokers_.find(key);
+    auto it = map_invokers_.find(std::string(key));
     if (it != map_invokers_.end()) {
       auto &pair = it->second;
       if (method[0] < 'A' || method[0] > 'Z')
